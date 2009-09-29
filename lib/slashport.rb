bd51bf1c691050d8914de368c815850016c41d6d
Jordan Sissel <jls@semicomplete.com>
2009-09-29 08:34:32 +0000

    - comment++formatting
    git-svn-id: http://semicomplete.googlecode.com/svn/slashport@2279 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- lib/slashport.rb
+++ lib/slashport.rb
@@ -1,7 +1,5 @@
-
 module SlashPort
   class Component
-
     # Keep a registry of subclasses for autoregistration
     class << self
       @@subclasses = []
@@ -14,7 +12,7 @@ module SlashPort
         if subclass.respond_to?(:class_initialize)
           subclass.class_initialize
         end
-      end # def inherited
+      end # def self.inherited
 
       # class-level to easily map a variable name to a method
       def variable(name, method)
@@ -23,22 +21,24 @@ module SlashPort
         self.class_eval do
           @variables[name] = method
         end
-      end
-    end # class << self (Component)
+      end # def self.variable
+ 
+      # class-level initialization. This is called when ruby first
+      # creates this class object, a hack made possible by
+      # overriding Class#inherited (see 'def inherited' above).
+      def class_initialize
+        puts "#{self}::class_initialize"
 
-    # hack to add a per-subclass @variables
-    def self.class_initialize
-      puts "#{self}::class_initialize"
+        self.class_eval do
+          @variables = Hash.new
+        end
+      end # def.class_initialize
 
-      self.class_eval do
-        @variables = Hash.new
-      end
-    end
-
-    # Show me all subclasses of SlashPort::Component
-    def self.all
-      return @@subclasses
-    end
+      # Show me all subclasses of SlashPort::Component
+      def all
+        return @@subclasses
+      end # def self.all
+    end # class << self (SlashPort::Component)
   end # module Component
 end # module SlashPort
 
